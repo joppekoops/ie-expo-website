3fbbaea8a6bf7ec766df07cf2bf06562fc7ffcbd
jkhvacmd209 <joppe.koops@hva.nl>
2024-01-19 11:31:25 +0100

    UX fix

--- js/script.js
+++ js/script.js
@@ -40,57 +40,57 @@ if(ticketButton) {
     }
 }
 
-const slider = document.querySelector('.slider')
-const slides = document.querySelectorAll('article.project')
-const prevButton = document.querySelector('.projects-prev-button')
-const nextButton = document.querySelector('.projects-next-button')
-let currentSlide = 0
+// const slider = document.querySelector('.slider')
+// const slides = document.querySelectorAll('article.project')
+// const prevButton = document.querySelector('.projects-prev-button')
+// const nextButton = document.querySelector('.projects-next-button')
+// let currentSlide = 0
 
-for (let i = 0; i < slides.length; i++) {
-    slides[i].style.transform = 'translateX(calc(' + i * 88 + 'vw + ' + i * 1.5 + 'rem))'
-}
+// for (let i = 0; i < slides.length; i++) {
+//     slides[i].style.transform = 'translateX(calc(' + i * 88 + 'vw + ' + i * 1.5 + 'rem))'
+// }
 
-let goToSlide = (nextSlide) => {
+// let goToSlide = (nextSlide) => {
 
-    slider.style.transform = 'translateX(calc(' + nextSlide * -88 + 'vw + ' + nextSlide * -1.5 + 'rem))'
+//     slider.style.transform = 'translateX(calc(' + nextSlide * -88 + 'vw + ' + nextSlide * -1.5 + 'rem))'
 
-    let nextTitle
-    let prevTitle
+//     let nextTitle
+//     let prevTitle
 
-    if (nextSlide == 0) {
-        prevButton.style.display = 'none';
-        nextButton.style.display = 'flex';
+//     if (nextSlide == 0) {
+//         prevButton.style.display = 'none';
+//         nextButton.style.display = 'flex';
 
-        nextTitle = slides[nextSlide].nextElementSibling.querySelector('h3').innerText
-    }
+//         nextTitle = slides[nextSlide].nextElementSibling.querySelector('h3').innerText
+//     }
 
-    else if (nextSlide == slides.length - 1) {
-        prevButton.style.display = 'flex';
-        nextButton.style.display = 'none';
+//     else if (nextSlide == slides.length - 1) {
+//         prevButton.style.display = 'flex';
+//         nextButton.style.display = 'none';
 
-        prevTitle = slides[nextSlide].previousElementSibling.querySelector('h3').innerText
-    }
+//         prevTitle = slides[nextSlide].previousElementSibling.querySelector('h3').innerText
+//     }
 
-    else {
-        prevButton.style.display = 'flex';
-        nextButton.style.display = 'flex';
+//     else {
+//         prevButton.style.display = 'flex';
+//         nextButton.style.display = 'flex';
 
-        prevTitle = slides[nextSlide].previousElementSibling.querySelector('h3').innerText
-        nextTitle = slides[nextSlide].nextElementSibling.querySelector('h3').innerText
-    }
+//         prevTitle = slides[nextSlide].previousElementSibling.querySelector('h3').innerText
+//         nextTitle = slides[nextSlide].nextElementSibling.querySelector('h3').innerText
+//     }
 
-    prevButton.firstElementChild.innerText = prevTitle
-    nextButton.firstElementChild.innerText = nextTitle
+//     prevButton.firstElementChild.innerText = prevTitle
+//     nextButton.firstElementChild.innerText = nextTitle
 
-    currentSlide = nextSlide
-}
+//     currentSlide = nextSlide
+// }
 
-prevButton.onclick = () => {
-    goToSlide(currentSlide-1)
-}
-nextButton.onclick = () => {
-    goToSlide(currentSlide+1)
-}
+// prevButton.onclick = () => {
+//     goToSlide(currentSlide-1)
+// }
+// nextButton.onclick = () => {
+//     goToSlide(currentSlide+1)
+// }
 
-goToSlide(0)
+// goToSlide(0)
 
